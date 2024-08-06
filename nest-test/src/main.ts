6e728be4437e9375084a68938c4d6f34d4a74515
thankslyh <liyinghao@shuidi-inc.com>
2024-08-06 20:29:25 +0800

    feat: 自动化流程

--- nest-test/src/main.ts
+++ nest-test/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('4396');
   await app.listen(4396);
 }
 bootstrap();
